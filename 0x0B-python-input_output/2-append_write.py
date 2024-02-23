8f3a04eec6da59b59e39f95b3535f993b19a3ddd
Asarefoster <asarefosterinnocent@gmail.com>
2024-02-23 04:41:52 +0000

    input output

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -4,12 +4,12 @@
 
 def append_write(filename="", text=""):
     """
-        Appends a string at the end of a text file (UTF8) and returns
-	    the number of characters added
+    Appends a string at the end of a text file (UTF8) and returns
+    the number of characters added
 
-	        Args:
-		    filename (string): the name of the file
-				           text (string): text to write to the file
-							      """
-								     with open(filename, 'a', encoding="utf-8") as f:
-										              return f.write(text)
+    Args:
+    filename (string): the name of the file
+    text (string): text to write to the file
+    """
+    with open(filename, 'a', encoding="utf-8") as f:
+        return f.write(text)
--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -2,18 +2,18 @@
 """
 Defines a function that returns a JSON representation
 of an object (string)
-	"""
+"""
 
 
-	def to_json_string(my_obj):
-		    """
-			        Returns a JSON representation of an object
+def to_json_string(my_obj):
+    """
+    Returns a JSON representation of an object
 
-				    Args:
-				        my_obj (object): string
+    Args:
+    my_obj (object): string
 
-							     Return:
-							         JSON representation of an object
-								     """
-								         import json
-									     return json.dumps(my_obj)
+    Return:
+    JSON representation of an object
+    """
+    import json
+    return json.dumps(my_obj)
